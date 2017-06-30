53638d1fe1a27cc01e2b8fe2b07dc1a725b8ebf7
bartoszmaka <bartoszmaka@gmail.com>
2017-06-30 15:35:33 +0200

    Make all specs pass

--- exercises/car_factory/car.rb
+++ exercises/car_factory/car.rb
@@ -1,4 +1,32 @@
 class Car
+  attr_reader :color_name , :brand
 
+  @@AVAILABLE_COLORS = [:black, :red, :yellow]
+  @@color_counter = 0
+
+  def initialize(brand, args={})
+    @brand = brand.to_s.capitalize
+    @color_name = parse_color(args[:color])
+  end
+
+  private
+
+  def parse_color(given_color)
+    given_color.nil? ? pick_color_from_avaible_colors : formatted_color_name(given_color)
+  end
+
+  def formatted_color_name(unformatted_data)
+    unformatted_data.to_s.split('_').map(&:capitalize).join(' ')
+  end
+
+  def pick_color_from_avaible_colors
+    picked_color = formatted_color_name(@@AVAILABLE_COLORS[@@color_counter])
+    tick_color_counter
+    picked_color
+  end
+
+  def tick_color_counter
+    @@color_counter = (@@color_counter + 1) % @@AVAILABLE_COLORS.length
+  end
 end
 
